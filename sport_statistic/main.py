ecc8656814b3e38cf0579da6f4c41eae9034ef2f
Alexander Kostarev <UniverAlex@ya.ru>
2017-12-24 17:53:11 +0300

    if applied, this commit will remove unused import

--- sport_statistic/main.py
+++ sport_statistic/main.py
@@ -1,7 +1,6 @@
 from sport_statistic import Gtk
 from sport_statistic.controller import Controller
 from sport_statistic.model import Model
-from sport_statistic.view import TreeViewWindow, EntryWindow
 
 Controller(Model())
 Gtk.main()
